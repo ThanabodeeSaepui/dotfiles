cbbe16a4af29116654070e8ba4c443007b70f1d2
thanabodeeSaepui <thanabodee.saepui@gmail.com>
2026-07-02 22:23:51 +0700

    fix: openldap

--- modules/overlay.nix
+++ modules/overlay.nix
@@ -3,5 +3,10 @@
   nixpkgs.overlays = [
     inputs.nur.overlays.default
     inputs.niri.overlays.niri
+    (final: prev: {
+      openldap = prev.openldap.overrideAttrs (old: {
+        doCheck = false;
+      });
+    })
   ];
 }
