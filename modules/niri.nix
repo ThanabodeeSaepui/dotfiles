c25c429787ebaafce3a7b916aa612eb30d79ca93
thanabodeeSaepui <thanabodee.saepui@gmail.com>
2026-09-24 15:58:29 +0700

    feat: update flake

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,5 +1,8 @@
 {
   programs.niri = {
     enable = true;
+    # Use the GTK portal directly; Nautilus pulls in gnome-user-share/Apache
+    # and is not otherwise used on this Niri setup.
+    useNautilus = false;
   };
 }
